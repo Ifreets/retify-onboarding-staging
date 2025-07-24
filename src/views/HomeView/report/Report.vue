1ab24673c12113e6b53457960fa48b5181b5d5c5
bbhhainx <bbhhainx>
2025-07-24 18:09:39 +0700

    report & setting

--- src/views/HomeView/report/Report.vue
+++ src/views/HomeView/report/Report.vue
@@ -1,3 +1,72 @@
-<template>report</template>
+<template>
+  <article class="h-full flex flex-col items-center justify-center relative">
+    <header class="flex justify-center w-full px-2 text-2xl font-semibold">
+      Analytics Dashboard
+    </header>
+    <main class="w-full h-full flex flex-col overflow-auto py-3">
+      <div class="px-2 flex flex-col gap-5">
+        <section class="border border-slate-200 rounded-lg p-3 flex flex-col gap-3">
+          <p class="text-base font-semibold">Action Items</p>
+          <div class="bg-slate-200 h-px"></div>
+          <div class="flex gap-3 text-red-500 font-semibold border bg-red-50 p-3 border-red-500 rounded-lg">
+            <ExclamationCircleIcon class="size-6 flex-shrink-0" />
+            <p class="flex-grow overflow-hidden">2 New Negative Feedbacks!</p>
+            <button class="py-1 px-2 rounded bg-red-500 text-white flex-shrink-0 h-fit">Review</button>
+          </div>
+          <div class="flex gap-3 text-red-500 font-semibold border bg-red-50 p-3 border-red-500 rounded-lg">
+            <ExclamationCircleIcon class="size-6 flex-shrink-0" />
+            <p class="flex-grow overflow-hidden">3 New Orders Need Confirmation</p>
+            <button class="py-1 px-2 rounded bg-red-500 text-white flex-shrink-0 h-fit">Confirm</button>
+          </div>
+          <div class="flex gap-3 text-blue-700 font-semibold border bg-blue-50 p-3 border-blue-700 rounded-lg">
+            <LightBulbIcon class="size-6 flex-shrink-0" />
+            <p class="flex-grow overflow-hidden">3 New Orders Need Confirmation</p>
+            <button class="py-1 px-2 rounded bg-blue-700 text-white flex-shrink-0 h-fit">Confirm</button>
+          </div>
+        </section>
+        <ul class="flex flex-col gap-5 mx-3">
+          <li class="flex gap-3 py-2 px-3 bg-slate-100 rounded-lg items-center">
+            <div class="bg-slate-200 rounded-lg p-2">
+              <ChatBubbleLeftRightIcon class="size-5 flex-shrink-0 text-slate-700" />
+            </div>
+            <div class="flex flex-col flex-grow">
+              <p class="font-medium">Total Messages</p>
+              <p class="text-base font-semibold">10.927</p>
+            </div>
+          </li>
+          <li class="flex gap-3 py-2 px-3 bg-slate-100 rounded-lg items-center">
+            <div class="bg-slate-200 rounded-lg p-2">
+              <FaceFrownIcon class="size-5 flex-shrink-0 text-red-500" />
+            </div>
+            <div class="flex flex-col flex-grow">
+              <div class="font-medium flex gap-2">
+                <p class="flex-grow truncate">Number of negative messages</p>
+                <ExternalLinkIcon class="size-4 flex-shrink-0 text-blue-700" />
+              </div>
+              <p class="text-base font-semibold text-red-500">16.482</p>
+            </div>
+          </li>
+          <li class="flex gap-3 py-2 px-3 bg-slate-100 rounded-lg items-center">
+            <div class="bg-slate-200 rounded-lg p-2">
+              <FaceSmileIcon class="size-5 flex-shrink-0 text-blue-700" />
+            </div>
+            <div class="flex flex-col flex-grow">
+              <div class="font-medium flex gap-2">
+                <p class="flex-grow truncate">Number of active messages</p>
+                <ExternalLinkIcon class="size-4 flex-shrink-0 text-slate-500" />
+              </div>
+              <p class="text-base font-semibold text-blue-700">16.482</p>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </main>
+  </article>
+</template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import ExternalLinkIcon from '@/components/icons/ExternalLinkIcon.vue';
+import LightBulbIcon from '@/components/icons/LightBulbIcon.vue';
+import { ExclamationCircleIcon } from '@heroicons/vue/24/outline'
+import { ChatBubbleLeftRightIcon, FaceFrownIcon, FaceSmileIcon } from '@heroicons/vue/24/solid';
+</script>
--- src/views/HomeView/setting/Setting.vue
+++ src/views/HomeView/setting/Setting.vue
@@ -1,9 +1,183 @@
 <template>
   <article class="h-full flex flex-col items-center justify-center relative">
-    setting
+    <header class="flex justify-center w-full px-2 border-b">
+      <h3 class="text-2xl font-semibold pb-2">Settings</h3>
+    </header>
+    <main class="w-full h-full my-3 gap-4 flex flex-col overflow-auto">
+      <section class="px-3 flex flex-col gap-2">
+        <p class="text-lg font-semibold sticky top-0 bg-white">Integrations</p>
+        <ul class="flex flex-col gap-4">
+          <li>
+            <div class="flex gap-3 items-center">
+              <div class="p-2 rounded-full bg-blue-100 w-fit h-fit">
+                <ShoppingBagIcon class="size-5 text-blue-700" />
+              </div>
+              <div class="flex-grow flex gap-6 items-center">
+                <div class="text-base flex-grow">
+                  <p class="font-semibold">POS System</p>
+                  <p>Connect with Clover, Square, and more</p>
+                </div>
+                <ChevronRightIcon class="size-5 text-slate-500 flex-shrink-0" />
+              </div>
+            </div>
+            <div class="ml-12 border-b h-4 border-slate-200"></div>
+          </li>
+          <li>
+            <div class="flex gap-3 items-center">
+              <div class="p-2 rounded-full bg-blue-100 w-fit h-fit">
+                <ChatBubbleLeftRightIcon class="size-5 text-blue-700" />
+              </div>
+              <div class="flex-grow flex gap-6 items-center">
+                <div class="text-base flex-grow">
+                  <p class="font-semibold">Messaging Platforms</p>
+                  <p>Connect with Clover, Square, and more</p>
+                </div>
+                <ChevronRightIcon class="size-5 text-slate-500 flex-shrink-0" />
+              </div>
+            </div>
+            <div class="ml-12 border-b h-4 border-slate-200"></div>
+          </li>
+        </ul>
+      </section>
+
+      <section class="px-3 flex flex-col gap-2">
+        <p class="text-lg font-semibold sticky top-0 bg-white">Merchant Information</p>
+        <ul class="flex flex-col gap-4">
+          <li>
+            <div class="flex gap-3 items-center">
+              <div class="p-2 rounded-full bg-blue-100 w-fit h-fit">
+                <BriefcaseIcon class="size-5 text-blue-700" />
+              </div>
+              <div class="flex-grow flex gap-6 items-center">
+                <div class="text-base flex-grow">
+                  <p class="font-semibold">Business Profile</p>
+                  <p>Operating hours, contact info, legal details, Parking info</p>
+                </div>
+                <ChevronRightIcon class="size-5 text-slate-500 flex-shrink-0" />
+              </div>
+            </div>
+            <div class="ml-12 border-b h-4 border-slate-200"></div>
+          </li>
+          <li>
+            <div class="flex gap-3 items-center">
+              <div class="p-2 rounded-full bg-blue-100 w-fit h-fit">
+                <CubeIcon class="size-5 text-blue-700" />
+              </div>
+              <div class="flex-grow flex gap-6 items-center">
+                <div class="text-base flex-grow">
+                  <p class="font-semibold">Product Information</p>
+                  <p>Product listing information for customers and AI training</p>
+                </div>
+                <ChevronRightIcon class="size-5 text-slate-500 flex-shrink-0" />
+              </div>
+            </div>
+            <div class="ml-12 border-b h-4 border-slate-200"></div>
+          </li>
+        </ul>
+      </section>
+
+      <section class="px-3 flex flex-col gap-2">
+        <p class="text-lg font-semibold sticky top-0 bg-white">AI & QR Code</p>
+        <ul class="flex flex-col gap-4">
+          <li>
+            <div class="flex gap-3 items-center">
+              <div class="p-2 rounded-full bg-blue-100 w-fit h-fit">
+                <SparklesIcon class="size-5 text-blue-700" />
+              </div>
+              <div class="flex-grow flex gap-6 items-center">
+                <div class="text-base flex-grow">
+                  <p class="font-semibold">AI Agent</p>
+                  <p>Setting AI Agento</p>
+                </div>
+                <ChevronRightIcon class="size-5 text-slate-500 flex-shrink-0" />
+              </div>
+            </div>
+            <div class="ml-12 border-b h-4 border-slate-200"></div>
+          </li>
+          <li>
+            <div class="flex gap-3 items-center">
+              <div class="p-2 rounded-full bg-blue-100 w-fit h-fit">
+                <QrCodeIcon class="size-5 text-blue-700" />
+              </div>
+              <div class="flex-grow flex gap-6 items-center">
+                <div class="text-base flex-grow">
+                  <p class="font-semibold">QR Code</p>
+                  <p>Download QR for AI customer chat</p>
+                </div>
+                <ChevronRightIcon class="size-5 text-slate-500 flex-shrink-0" />
+              </div>
+            </div>
+            <div class="ml-12 border-b h-4 border-slate-200"></div>
+          </li>
+        </ul>
+      </section>
+
+      <section class="px-3 flex flex-col gap-2">
+        <p class="text-lg font-semibold sticky top-0 bg-white">Legal & Support</p>
+        <ul class="flex flex-col gap-4">
+          <li>
+            <div class="flex gap-3 items-center">
+              <div class="p-2 rounded-full bg-blue-100 w-fit h-fit">
+                <LockClosedIcon class="size-5 text-blue-700" />
+              </div>
+              <div class="flex-grow flex gap-6 items-center">
+                <div class="text-base flex-grow">
+                  <p class="font-semibold">Privacy Policy</p>
+                  <p>View the application's privacy policy</p>
+                </div>
+                <ChevronRightIcon class="size-5 text-slate-500 flex-shrink-0" />
+              </div>
+            </div>
+            <div class="ml-12 border-b h-4 border-slate-200"></div>
+          </li>
+          <li>
+            <div class="flex gap-3 items-center">
+              <div class="p-2 rounded-full bg-blue-100 w-fit h-fit">
+                <DocumentTextIcon class="size-5 text-blue-700" />
+              </div>
+              <div class="flex-grow flex gap-6 items-center">
+                <div class="text-base flex-grow">
+                  <p class="font-semibold">Terms of Service</p>
+                  <p>Read the terms and conditions of service</p>
+                </div>
+                <ChevronRightIcon class="size-5 text-slate-500 flex-shrink-0" />
+              </div>
+            </div>
+            <div class="ml-12 border-b h-4 border-slate-200"></div>
+          </li>
+          <li>
+            <div class="flex gap-3 items-center">
+              <div class="p-2 rounded-full bg-blue-100 w-fit h-fit">
+                <LifebuoyIcon class="size-5 text-blue-700" />
+              </div>
+              <div class="flex-grow flex gap-6 items-center">
+                <div class="text-base flex-grow">
+                  <p class="font-semibold">Get Support</p>
+                  <p>Get Support</p>
+                </div>
+                <ChevronRightIcon class="size-5 text-slate-500 flex-shrink-0" />
+              </div>
+            </div>
+            <div class="ml-12 border-b h-4 border-slate-200"></div>
+          </li>
+        </ul>
+      </section>
+    </main>
+    <p class="py-3 text-slate-500">Version 1.0.0 (Build 20240726)</p>
   </article>
 </template>
-  
-<script setup lang='ts'>
-  
+
+<script setup lang="ts">
+import QrCodeIcon from '@/components/icons/QrCodeIcon.vue';
+import {
+  BriefcaseIcon,
+  ChatBubbleLeftRightIcon,
+  ChevronRightIcon,
+  CubeIcon,
+  DocumentTextIcon,
+  LifebuoyIcon,
+  LockClosedIcon,
+  ShoppingBagIcon,
+  SparklesIcon,
+} from '@heroicons/vue/24/solid';
 </script>
