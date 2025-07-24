0783e0e278511e26172866f493b4b21f0d12beba
manhbbh <manhtechon@gmail.com>
2025-07-24 15:55:29 +0700

    Deploy vercel bởi bbhhainx thông qua manhbbh

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,5 @@
 <template>
-  <main class="h-dvh w-dvw flex bg-white flex-col max-w-md mx-auto text-sm py-2">
+  <main class="h-dvh w-dvw flex bg-white flex-col max-w-md mx-auto text-sm pt-2">
     <div class="h-full overflow-hidden">
       <RouterView />
     </div>
