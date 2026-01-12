3009a0494f3329000b37570314cfc1ec604ee5c3
Le Gia Uy <leuy091104@gmail.com>
2026-01-12 16:32:27 +0700

    style: chỉnh vị trí 2 nút pay và print

--- src/views/HomeView/order/OrderDetail.vue
+++ src/views/HomeView/order/OrderDetail.vue
@@ -13,7 +13,7 @@
         Orders #{{ route.params.id }}
       </h3>
       <!-- Nút Pay và Print bên phải -->
-      <div class="flex gap-1.5 z-10">
+      <div class="relative top-6 right-4 flex gap-1.5 z-10">
         <!-- Nút Pay -->
         <button
           class="flex items-center gap-1 py-1 px-2 bg-blue-600 text-white rounded-md font-medium text-xs hover:bg-blue-700 transition-colors"
